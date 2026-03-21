1fc45733582238e106dad39ac4cc23c9c189782a
Jona3333 <jonathan.etin13@gmail.com>
2026-03-21 16:30:51 +0200

    edge detection and other bs

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
 import cv2
-image1= cv2.imread('imtest1.JPG')
-image2= cv2.imread('imtest2.JPG')
+import numpy as np
+#from skimage import io, segmentation, color, measure
+#from skimage import graph
+#import matplotlib.pyplot as plt
 
-image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-subtracted = cv2.subtract( image2, image1)
-# to convert to numpy use imread ('image' mode= 'RGB')
-cv2.imshow('image', subtracted)
+def edge_detection(img1_path, img2_path):
+    image1= cv2.imread('imtest1.JPG')
+    image2= cv2.imread('imtest2.JPG')
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Read the original image
+    img = subtracted = cv2.subtract( image2, image1)
+
+    # Convert to graycsale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Blur the image for better edge detection
+    img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
+
+    # Sobel Edge Detection
+    sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)  # Sobel Edge Detection on the X axis
+    sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)  # Sobel Edge Detection on the Y axis
+    sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)  # Combined X and Y Sobel Edge Detection
+
+    # Canny Edge Detection
+    edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)  # Canny Edge Detection
+
+    edges_arr = np.array(edges)
+    edges_arr = edges_arr // 255
+
+    return edges_arr
